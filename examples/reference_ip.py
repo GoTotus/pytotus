a9d01e913349e8f82c841e3c7f2d16221ca2ff61
Eduardo ES Riccardi <ed@kukino.uk>
2025-03-03 18:22:37 -0500

    simpler

--- examples/reference_ip.py
+++ examples/reference_ip.py
@@ -7,10 +7,13 @@ t = Totus()
 reference = t.Reference()
 
 print("Your Public IP ...")
-print(json.dumps(reference.IP(), indent=4))
+result = reference.IP()
+print(json.dumps(result, indent=4))
 
 print("Cloudflare 1.1.1.1 ...")
-print(json.dumps(reference.IP(ip4='1.1.1.1'), indent=4))
+result = reference.IP(ip4='1.1.1.1')
+print(json.dumps(result, indent=4))
 
 print("Cloudflare ip6 for previous 1.1.1.1: 2606:4700:4700::1111 ...")
-print(json.dumps(reference.IP(ip6='2606:4700:4700::1111'), indent=4))
+result = reference.IP(ip6='2606:4700:4700::1111')
+print(json.dumps(result, indent=4))
